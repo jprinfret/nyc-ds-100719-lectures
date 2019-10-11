b4e56d943e9b90a76397ce648272a03b5415c12e
JP Rinfret <johnpaul.rinfret@gmail.com>
2019-10-11 09:03:26 -0400

    101119 0800AM edits

--- week-1/OOP/calculator_class/descriptive.py
+++ week-1/OOP/calculator_class/descriptive.py
@@ -11,43 +11,42 @@ import math
 class Calculator:
 
     def __init__(self, data):
-        self.data = data
+        self.data = sorted(data)
+        self.__calc_metrics__()
         
-    def __calc_metrics__(self)    
-        self.length = self.__getLength__()
+    def __calc_metrics__(self):    
+        self.length = len(self.data)
+        self.total = sum(self.data)
         self.mean = self.__getMean__()
         self.median = self.__getMedian__()
         self.variance = self.__getVariance__()
         self.stand_dev = self.__getStand_Dev__()
     
-    def __getLength__(self):
-        return len(self.data)
-    
     def __getMean__(self):
-        return sum(self.data)/self.length
+        return self.total/self.length
     
     def __getMedian__(self):
         if self.length % 2:
             return self.data[self.length//2]
         else:
-            return (self.data[self.length//2] + 
-                              self.data[self.length//2]-1)/2
+            return (self.data[self.length//2] + self.data[self.length//2]-1)/2
     
     def __getVariance__(self):
         numerator = 0
         for i in self.data:
             numerator += (i - self.mean)**2
-            return numerator/self.length
+        return numerator/(self.length - 1)
     
     def __getStand_Dev__(self):
         return math.sqrt(self.variance)
     
     def add_data(self, new_data):
         self.data.extend(new_data)
-        __calc_metrics__(self)
+        self.data = sorted(self.data)
+        self.__calc_metrics__()
     
     def remove_data(self, new_data):
         for i in new_data:
             if i in self.data:
                 self.data.remove(i)
-        __calc_metrics__(self)
+        self.__calc_metrics__()
